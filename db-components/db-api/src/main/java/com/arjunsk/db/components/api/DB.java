1f38c93adca3090abec9855e5be9fb1e719d4f52
Arjun SK <arjun.sunilkumar@nissanmotor.com>
2021-03-21 20:47:37 +0530

    Added WriteBatch

--- db-components/db-api/src/main/java/com/arjunsk/db/components/api/DB.java
+++ db-components/db-api/src/main/java/com/arjunsk/db/components/api/DB.java
@@ -11,4 +11,21 @@ public interface DB extends Iterable<Map.Entry<byte[], byte[]>>, Closeable {
   void put(byte[] key, byte[] value) throws DBException;
 
   void delete(byte[] key) throws DBException;
+
+  /**
+   * ### BATCH SUPPORT ###
+   *
+   * <p>Usage:
+   *
+   * <pre>
+   *  WriteBatch batch = db.createWriteBatch();
+   *  batch.put(bytes("key1"),bytes("value1"));
+   *  batch.put(bytes("key2"),bytes("value2"));
+   *  batch.delete(bytes("key3"));
+   *  db.write(batch);
+   * </pre>
+   */
+  WriteBatch createWriteBatch();
+
+  void write(WriteBatch updates) throws DBException;
 }
